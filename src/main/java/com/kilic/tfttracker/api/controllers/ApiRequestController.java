5898b14d4d8253325f84e5ae309fa1f61dabb74f
Isa Kilic <Is4Kilic@hotmail.com>
2023-06-16 11:57:36 +0200

    inital commit

---
+++ src/main/java/com/kilic/tfttracker/api/controllers/ApiRequestController.java
@@ -0,0 +1,21 @@
+package com.kilic.tfttracker.api.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tft")
+public class ApiRequestController {
+
+    Logger logger = LoggerFactory.getLogger(ApiRequestController.class);
+
+    @GetMapping("/matchhistory/{user}")
+    public @ResponseBody void getUserHistory(@PathVariable String username) {
+        logger.info("{}", username);
+    }
+}
