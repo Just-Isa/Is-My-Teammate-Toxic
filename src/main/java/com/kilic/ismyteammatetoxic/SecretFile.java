8fe1600db1f4f8911a0560b441cae4195a3402b2
Isa Kilic <Is4Kilic@hotmail.com>
2023-11-19 20:30:01 +0100

    FEAT/added env for api key

--- src/main/java/com/kilic/ismyteammatetoxic/SecretFile.java
+++ src/main/java/com/kilic/ismyteammatetoxic/SecretFile.java
@@ -1,10 +1,15 @@
 package com.kilic.ismyteammatetoxic;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import no.stelar7.api.r4j.basic.APICredentials;
 
 public class SecretFile {
 
-    public static final String LEAGUE_KEY = "RGAPI-96f0a4c2-cefb-4ca6-9ac7-6d5c7b2e0cd8";
+    //RGAPI-6acaefcf-73da-4ee7-9e2a-75e25e817313
+
+    @Value("${riot_api_key}")
+    public static String LEAGUE_KEY;
     public static final String TOURNAMENT_KEY = "NO_KEY_:(";
     public static final String LOR_KEY = "NO_KEY_:(";
     public static final String TFT_KEY = "NO_KEY_:(";
