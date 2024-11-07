769b480f3a931cdd2f5204bd51f9109162c2f6bf
Isa Kilic <Is4Kilic@hotmail.com>
2024-11-07 16:24:47 +0100

    bugfix: api stuff...

--- src/main/java/com/kilic/ismyteammatetoxic/SecretFile.java
+++ src/main/java/com/kilic/ismyteammatetoxic/SecretFile.java
@@ -1,19 +1,25 @@
 package com.kilic.ismyteammatetoxic;
 
-
 import org.springframework.beans.factory.annotation.Value;
-
 import no.stelar7.api.r4j.basic.APICredentials;
 
 public class SecretFile {
 
-    @Value("${riot_api_key}")
-    public String LEAGUE_KEY;
+    public static String LEAGUE_KEY;
     public static final String TOURNAMENT_KEY = "NO_KEY_:(";
     public static final String LOR_KEY = "NO_KEY_:(";
     public static final String TFT_KEY = "NO_KEY_:(";
     public static final String VAL_KEY = "NO_KEY_:(";
 
+    @Value("${riot_api_key}")
+    public void setLeagueKey(String name) {
+        LEAGUE_KEY = name;
+    }
 
-    public final APICredentials CREDS = new APICredentials(LEAGUE_KEY);
+    public static String getKey(){
+        return LEAGUE_KEY;
+    }
+
+    public static final APICredentials CREDS = new APICredentials(getKey(), TOURNAMENT_KEY, TFT_KEY, LOR_KEY,
+            VAL_KEY);
 }
