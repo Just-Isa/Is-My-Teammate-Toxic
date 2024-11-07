bf5206047d551b2f1315f3ed8054b147cbc3d6b4
Isa Kilic <Is4Kilic@hotmail.com>
2024-11-07 16:18:20 +0100

    bugfix: value doesnt work with static fields...

--- src/main/java/com/kilic/ismyteammatetoxic/SecretFile.java
+++ src/main/java/com/kilic/ismyteammatetoxic/SecretFile.java
@@ -8,11 +8,12 @@ import no.stelar7.api.r4j.basic.APICredentials;
 public class SecretFile {
 
     @Value("${riot_api_key}")
-    public static String LEAGUE_KEY;
+    public String LEAGUE_KEY;
     public static final String TOURNAMENT_KEY = "NO_KEY_:(";
     public static final String LOR_KEY = "NO_KEY_:(";
     public static final String TFT_KEY = "NO_KEY_:(";
     public static final String VAL_KEY = "NO_KEY_:(";
 
-    public static final APICredentials CREDS = new APICredentials(LEAGUE_KEY);
+
+    public final APICredentials CREDS = new APICredentials(LEAGUE_KEY);
 }
