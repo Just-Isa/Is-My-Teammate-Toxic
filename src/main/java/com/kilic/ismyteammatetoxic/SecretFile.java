3038f53c70b696860b2c261bd77a077a9a1d5249
Isa Kilic <Is4Kilic@hotmail.com>
2024-11-09 18:01:29 +0100

    refactor: api variable

--- src/main/java/com/kilic/ismyteammatetoxic/SecretFile.java
+++ src/main/java/com/kilic/ismyteammatetoxic/SecretFile.java
@@ -5,21 +5,14 @@ import no.stelar7.api.r4j.basic.APICredentials;
 
 public class SecretFile {
 
+    @Value("riot_api_key")
     public static String LEAGUE_KEY;
     public static final String TOURNAMENT_KEY = "NO_KEY_:(";
     public static final String LOR_KEY = "NO_KEY_:(";
     public static final String TFT_KEY = "NO_KEY_:(";
     public static final String VAL_KEY = "NO_KEY_:(";
 
-    @Value("${riot_api_key}")
-    public void setLeagueKey(String riot_api_key) {
-        LEAGUE_KEY = riot_api_key;
-    }
 
-    public static String getKey(){
-        return LEAGUE_KEY;
-    }
-
-    public static final APICredentials CREDS = new APICredentials(getKey(), TOURNAMENT_KEY, TFT_KEY, LOR_KEY,
-            VAL_KEY);
+    public static final APICredentials CREDS = new APICredentials(LEAGUE_KEY, TOURNAMENT_KEY, TFT_KEY, LOR_KEY,
+    VAL_KEY);
 }
