d52797f8990c7040b91a8be86ae3af5104a22df6
Isa Kilic <Is4Kilic@hotmail.com>
2024-11-09 18:52:31 +0100

    REFACTOR: work please... api key...

--- src/main/java/com/kilic/ismyteammatetoxic/SecretFile.java
+++ src/main/java/com/kilic/ismyteammatetoxic/SecretFile.java
@@ -1,18 +1,14 @@
 package com.kilic.ismyteammatetoxic;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Value;
 import no.stelar7.api.r4j.basic.APICredentials;
 
 public class SecretFile {
 
-    @Value("${LEAGUE_KEY}")
-    public static String LEAGUE_KEY;
-    public static final String TOURNAMENT_KEY = "NO_KEY_:(";
-    public static final String LOR_KEY = "NO_KEY_:(";
-    public static final String TFT_KEY = "NO_KEY_:(";
-    public static final String VAL_KEY = "NO_KEY_:(";
+    public static String LEAGUE_KEY = System.getenv("riot_api_key");
 
 
-    public static final APICredentials CREDS = new APICredentials(LEAGUE_KEY, TOURNAMENT_KEY, TFT_KEY, LOR_KEY,
-    VAL_KEY);
+    public final static APICredentials CREDS = new APICredentials(LEAGUE_KEY);
 }
