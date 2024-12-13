52411807d359f7bc3d79748fcaa27f0e6236d5be
Isa Kilic <Is4Kilic@hotmail.com>
2024-12-13 10:44:10 +0100

    bugfix: red side base now on correct position

--- frontend/src/components/DeathHeatmap/DeathHeatmap.vue
+++ frontend/src/components/DeathHeatmap/DeathHeatmap.vue
@@ -20,7 +20,7 @@
       <div v-else-if="
         teamType[teamColor] == 'Red'
         ">
-        <baseB style="transform: translate(670px,  200px); font-size: 30px; color: red;"/>
+        <baseB style="transform: translate(705px,  -23px); font-size: 30px; color: red;"/>
       </div>
       <div v-else style="color: white;">
         {{ teamType[teamColor] }}
